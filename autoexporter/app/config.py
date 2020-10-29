79bd6e5c0266f68a0919d5fd4d11392cc5413dbc
Simon Beginn <simon@simonmicro.de>
2020-10-29 16:32:06 +0100

    Moved to subdirs and improved random id

--- autoexporter/app/config.py
+++ autoexporter/app/config.py
@@ -4,10 +4,10 @@ import logging
 
 dirConfig = {
     'upload': tempfile.mkdtemp(),
-    'queue': os.path.join(os.getcwd(), 'queued'),
-    'working': os.path.join(os.getcwd(), 'working'),
-    'success': os.path.join(os.getcwd(), 'done', 'success'),
-    'failure': os.path.join(os.getcwd(), 'done', 'failure')
+    'queue': os.path.join(os.getcwd(), 'data', 'queued'),
+    'working': os.path.join(os.getcwd(), 'data', 'working'),
+    'success': os.path.join(os.getcwd(), 'data', 'ok'),
+    'failure': os.path.join(os.getcwd(), 'data', 'failed')
 }
 logging.debug(dirConfig)
 
